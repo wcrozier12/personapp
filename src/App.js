459e2bf6d3d16410831f8923276b258d057ea073
wcrozier12 <wcc8@nau.edu>
2017-11-16 15:31:42 -0700

    removed radium

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
   state = {
@@ -54,11 +53,7 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
+      cursor: 'pointer'
     };
 
     let persons = null;
@@ -77,10 +72,6 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'Red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
     };
 
     const classes = [];
@@ -91,7 +82,6 @@ class App extends Component {
       classes.push('bold');
     }
     return (
-      <StyleRoot>
         <div className="App">
           <h1>Hi, I'm a react app</h1>
           <p className={classes.join(' ')}>This is really working!</p>
@@ -100,9 +90,8 @@ class App extends Component {
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
         </div>
-      </StyleRoot>
     );
   }
 }
 
-export default Radium(App);
+export default App;
