29430794489ce2c5a7aef4bccd2294900d35bc87
wcrozier12 <wcc8@nau.edu>
2017-11-16 15:50:56 -0700

    added button styling

--- src/App.js
+++ src/App.js
@@ -47,17 +47,8 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
-
+    let btnClass = '';
     if (this.state.showPersons) {
       persons = (
         <div>
@@ -71,7 +62,7 @@ class App extends Component {
         })}
         </div>
       );
-      style.backgroundColor = 'Red';
+      btnClass= classes.Red;
     };
 
     const assignedClasses = [];
@@ -85,8 +76,8 @@ class App extends Component {
         <div className={classes.App}>
           <h1>Hi, I'm a react app</h1>
           <p className={assignedClasses.join(' ')}>This is really working!</p>
-          <button 
-          style={style}
+          <button
+          className = {btnClass} 
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
         </div>
